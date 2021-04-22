0858ab1c5a1056f4b6f59c814bebf18a7f65cc8b
lizaferguson <lizaferguson19@gmail.com>
2021-04-21 22:03:59 -0400

    updated file structure

--- server.js
+++ server.js
@@ -48,11 +48,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, '/notes.html'))
 });
 
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, './public/index.html'));
+   res.sendFile(path.join(__dirname, '/index.html'));
 });   
 
 app.listen(PORT, () => {
